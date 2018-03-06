733be68284ddf21f32ea96fe6f8d69bcaf1610a8
Brian Bycynski <high5bri@hotmail.com>
2018-03-06 12:34:35 -0500

    still testing

--- js/__tests__/Search.spec.jsx
+++ js/__tests__/Search.spec.jsx
@@ -13,27 +13,24 @@ test("Search renders correctly", () => {
   expect(component).toMatchSnapshot();
 });
 
-xtest("Search should render correct amount of shows", () => {
+test("Search should render correct amount of shows", () => {
   const component = shallow(
-    <UnwrappedSearch shows={preload.shows} search="" />
+    <UnwrappedSearch shows={preload.shows} searchTerm="" />
   );
   expect(component.find(ShowCard).length).toEqual(preload.shows.length);
 });
 
-xtest(
-  "Search should render correct amount of shows based on search term",
-  () => {
-    const searchWord = "black";
-    const component = shallow(<Search shows={preload.shows} />);
-    component
-      .find("input")
-      .simulate("change", { target: { value: searchWord } });
-    const showCount = preload.shows.filter(
-      show =>
-        `${show.title} ${show.description}`
-          .toUpperCase()
-          .indexOf(searchWord.toUpperCase()) >= 0
-    ).length;
-    expect(component.find(ShowCard).length).toEqual(showCount);
-  }
-);
+test("Search should render correct amount of shows based on search term", () => {
+  const searchWord = "black";
+  const component = shallow(
+    <UnwrappedSearch shows={preload.shows} searchTerm={searchWord} />
+  );
+  // component.find("input").simulate("change", { target: { value: searchWord } });
+  const showCount = preload.shows.filter(
+    show =>
+      `${show.title} ${show.description}`
+        .toUpperCase()
+        .indexOf(searchWord.toUpperCase()) >= 0
+  ).length;
+  expect(component.find(ShowCard).length).toEqual(showCount);
+});
